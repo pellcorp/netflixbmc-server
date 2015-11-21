f748aef321635cf54d851821d0752920736f0270
Jason Pell <jason@pellcorp.com>
2015-11-21 23:45:25 +1100

    updated the url to watch a video

--- browser.sh
+++ browser.sh
@@ -19,7 +19,9 @@ fi
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-url=https://netflix.com/WiPlayer?movieid=$1
+url=https://netflix.com/watch/$1
+
+echo "URL: $url"
 
 # notice the ampersand to send google chrome into back ground so that the script continues and we execute the xdotool below
 /usr/bin/google-chrome --start-maximized --disable-translate --disable-new-tab-first-run --no-default-browser-check --no-first-run --kiosk "$url" & >/dev/null 2>&1
